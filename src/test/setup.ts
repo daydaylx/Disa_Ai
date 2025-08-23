3683dc9fb9bc370b0e72680ebe38042ddef83230
dlx <daydayfay05@gmail.com>
2025-08-23 09:06:35 +0200

    chore(test): fix import order/blank lines in test setup to satisfy linter

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,7 @@
 import "@testing-library/jest-dom";
+
 import { afterAll, afterEach, beforeAll, vi } from "vitest";
+
 import { server } from "./testServer";
 
 // MSW (Mock Service Worker) für fetch-Mocks
