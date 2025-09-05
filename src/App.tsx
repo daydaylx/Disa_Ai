e1037cd0d16df1b9a88b754bcdfd8ff3d8d454d7
dlx <daydayfay05@gmail.com>
2025-09-05 22:21:11 +0200

    fix(lint): no-empty catches in featureFlags; silence unused var in ChatView (_count)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/layout.css";
+
+import React, { lazy,Suspense } from "react";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+
+import TabBar from "./components/nav/TabBar";
 import { ToastsProvider } from "./components/ui/toast/ToastsProvider";
 import ChatView from "./views/ChatView";
-import SettingsView from "./views/SettingsView";
-import "./styles/layout.css";
-import TabBar from "./components/nav/TabBar";
+const SettingsView = lazy(() => import("./views/SettingsView"));
 
 const App: React.FC = () => (
   <div className="app-bg min-h-dvh text-white">
     <BrowserRouter>
       <ToastsProvider>
-        <Routes>
-          <Route path="/" element={<ChatView />} />
-          <Route path="/settings" element={<SettingsView />} />
-        </Routes>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<ChatView />} />
+            <Route path="/settings" element={<SettingsView />} />
+          </Routes>
+        </Suspense>
         <TabBar />
       </ToastsProvider>
     </BrowserRouter>
