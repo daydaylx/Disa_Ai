4c010f3fc347f234cbc30523f99abd0269755526
dlx <daydayfay05@gmail.com>
2025-09-16 02:38:17 +0200

    chore(build): run bundle analyzer in static mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import { analyzer } from "vite-bundle-analyzer";
 
+const analyzerPlugin = analyzer({
+  analyzerMode: process.env.BUNDLE_ANALYZE_MODE ?? "static",
+  openAnalyzer: false,
+  enabled: process.env.BUNDLE_ANALYZE !== "false",
+});
+
 export default defineConfig({
-  plugins: [react(), analyzer()],
+  plugins: [react(), analyzerPlugin],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
